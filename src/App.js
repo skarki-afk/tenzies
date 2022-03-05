236afd69588d75fdcff2e1e8c175ecd3deeb11bf
Sagar Karki <sagar.karki200@gmail.com>
2022-03-05 09:06:50 +0545

    except high score and responsive all finished

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Confetti from "react-confetti"
 const App =()=>{
   const generateNewDie =()=>{
     return {
-      value: Math.floor(Math.random() * 6 ) + 1,
+      value: Math.floor(Math.random() * 1 ) + 1,
       isHeld: false,
       id: nanoid()
     }
@@ -22,19 +22,52 @@ const App =()=>{
   
   const [dice,setDice] = React.useState(dieValue())
   const [tenzies,setTenzies] = React.useState(false)
+  const [score,setScore] = React.useState(0)
+  const [highScore,setHighScore]= React.useState([])
+
+ 
 
   React.useEffect( ()=>{
     const allHeld = dice.every(die=> die.isHeld)
     const firstValue = dice[0].value
     const allValue = dice.every(die=>die.value===firstValue)
     if(allHeld && allValue){
-      console.log("You Won!!")
       setTenzies(true)
+      console.log(score)
+      highScore.push(score)
     }else{
       setTenzies(false)
     }
   },
   [dice])
+
+
+  // React.useEffect( ()=>{
+  //   const allHeld = dice.every(die=> die.isHeld)
+  //   const firstValue = dice[0].value
+  //   const allValue = dice.every(die=>die.value===firstValue)
+  //   if(allHeld && allValue){
+  //     // setHighScore(prevHighScore => prevHighScore.push(score))
+  //     highScore.push(score)
+  //   }
+  // },
+  // [dice])
+
+  React.useEffect (()=>{
+    const scores = JSON.parse(localStorage.getItem("scores"))
+    if(scores){
+      setHighScore(scores)
+    }
+  },[])
+
+  React.useEffect (()=>{
+    localStorage.setItem("scores",JSON.stringify(highScore))
+    },[highScore])
+
+  
+
+  
+
   const holdDice =(id)=>{
     setDice(prevDice => prevDice.map(die=>
       id === die.id? 
@@ -43,16 +76,24 @@ const App =()=>{
       die
       ))
   }
-
+  
+  
   const diceElements = dice.map(
     die => <Dice 
-        key = {die.id}
-        isHeld = {die.isHeld} 
-        value = {die.value}
-        id={die.id}
-        onClick={()=>holdDice(die.id)}/>   
+       key = {die.id}
+       isHeld = {die.isHeld} 
+       value = {die.value}
+      id={die.id}
+      onClick={()=>holdDice(die.id)}/>   
       
-    )
+  )
+
+    
+  const countScore =()=>{
+    setScore(prevScore => prevScore + 1)
+    }
+    
+  
 
   const rollDice=()=>{
     if(!tenzies)
@@ -63,29 +104,40 @@ const App =()=>{
       ))}else{
         setTenzies(false)
         setDice(dieValue())
-      }
-  }
-  
-  
-  return(
-    <main>
-      <div className="app-body">
-        <h2>Tenzies</h2>
-        <p>Roll until all dice are same. Click each die to freeze it at its current value between rolls.</p>
-        <div 
-        className="die-body"
-        >
-          {diceElements}    
-        </div>
-        {tenzies && <Confetti/>}
-        <button
-          onClick={rollDice}
-         className="btn"> 
-          {tenzies? "New Game" : "Roll"}
-        </button>
+        setScore(-1)
+    }
+    }
+    // const highScoreNum = setHighScore(prevHighScore => Math.min(...prevHighScore))
 
+    // <h5>High Score:{highScoreNum}{highScoreNum > 1? "turns":"turn"}</h5> 
+  // console.log(highScore)
+    // console.log(typeof highScore)
+
+  return(
+      <div className="main">
+        <div className="app-body">
+          <div className="navbar">
+            <h3>Your Score: {score} {score>1?"turns":"turn"}</h3>
+            <h5>High Score: </h5>
+          </div>
+          <h2>Tenzies</h2>
+          <p>{tenzies? "You Won!!!":"Roll until all dice are same. Click each die to freeze it at its current value between rolls."}</p>
+          <div 
+          className="die-body"
+          >
+            {diceElements}    
+          </div>
+          {tenzies && <Confetti/>}
+          <button
+            onClick={ ()=> 
+              {rollDice()
+              countScore()}}
+          className="btn"> 
+            {tenzies? "New Game" : "Roll"}
+          </button>
+
+        </div>
       </div>
-    </main>
   )
 }
 
